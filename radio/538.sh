3fe782c197246cc231797272c707f4a21dbbb32a
Adriaan van Rossum <git@adriaan.io>
2015-11-28 18:48:35 +0100

    Fix radio 538 stream

--- radio/538.sh
+++ radio/538.sh
@@ -3,7 +3,8 @@
 source /root/cronjobs/tools/init.sh
 
 radio_name="538"
-radio_stream="http://82.201.100.9:8000/radio538"
+radio_stream="http://vip-icecast.538.lw.triple-it.nl:80/RADIO538_MP3"
+
 output_bitrate="128k"
 
 source /root/cronjobs/tools/record.sh
