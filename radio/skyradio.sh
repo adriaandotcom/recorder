5ad70d936a9ce1f4bb3570e33831163a1977d0ce
Adriaan <adriaanvanrossum@users.noreply.github.com>
2017-12-16 16:54:43 -0500

    Update stream URL of Skyradio with redirect

--- radio/skyradio.sh
+++ radio/skyradio.sh
@@ -3,7 +3,7 @@
 source /root/cronjobs/tools/init.sh
 
 radio_name="skyradio"
-radio_stream="http://20103.live.streamtheworld.com/SKYRADIOAAC.aac"
+radio_stream="http://playerservices.streamtheworld.com/api/livestream-redirect/SKYRADIO.mp3"
 output_bitrate="128k"
 
 source /root/cronjobs/tools/record.sh
