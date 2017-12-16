fc4b6a691917691a130d1ab030d4e81e3ddbf3e9
Adriaan <adriaanvanrossum@users.noreply.github.com>
2017-12-16 16:15:12 -0500

    Update stream URL of 100%NL

--- radio/100nl.sh
+++ radio/100nl.sh
@@ -3,7 +3,7 @@
 source /root/cronjobs/tools/init.sh
 
 radio_name="100nl"
-radio_stream="http://stream.100p.nl:8000/100pctnl.mp3"
+radio_stream="https://19013.live.streamtheworld.com/100PNL_AAC.aac"
 output_bitrate="128k"
 
 source /root/cronjobs/tools/record.sh
