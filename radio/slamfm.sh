b776f4750711c460a65607dc1a02d3d7be0c9cfd
Adriaan <adriaanvanrossum@users.noreply.github.com>
2017-12-16 15:14:42 -0500

    Update new stream URL for SLAM

--- radio/slamfm.sh
+++ radio/slamfm.sh
@@ -3,7 +3,7 @@
 source /root/cronjobs/tools/init.sh
 
 radio_name="slamfm"
-radio_stream="http://vip-icecast.538.lw.triple-it.nl:80/SLAMFM_MP3"
+radio_stream="https://hls.slam.nl/streaming/hls/SLAM\!/playlist.m3u8"
 output_bitrate="128k"
 
 source /root/cronjobs/tools/record.sh
