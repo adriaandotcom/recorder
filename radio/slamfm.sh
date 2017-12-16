0f51fdc0f4182e99f011a9ee66491330c3b7ab9b
Adriaan <adriaanvanrossum@users.noreply.github.com>
2017-12-16 15:38:57 -0500

    Update slamfm.sh

--- radio/slamfm.sh
+++ radio/slamfm.sh
@@ -3,7 +3,9 @@
 source /root/cronjobs/tools/init.sh
 
 radio_name="slamfm"
-radio_stream="https://hls.slam.nl/streaming/hls/SLAM\!/playlist.m3u8"
+radio_stream="https://hls.slam.nl/streaming/hls/SLAM%21/playlist.m3u8"
+params="-vn" # Skip video completely
+map="0:1" # Get second stream (which is audio)
 output_bitrate="128k"
 
 source /root/cronjobs/tools/record.sh
