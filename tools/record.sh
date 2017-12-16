e8392be43340fc7f7fee83dbaba4d86004d85acd
Adriaan <adriaanvanrossum@users.noreply.github.com>
2017-12-16 15:37:36 -0500

    Update record.sh

--- tools/record.sh
+++ tools/record.sh
@@ -3,4 +3,6 @@ cd /root/recordings/$radio_name
 
 # record for one hour
 timestamp=`date +%s`
-avconv -i "$radio_stream" -t 3600 -c:a libmp3lame -b:a $output_bitrate -map 0:0 -f segment -segment_time 300 -segment_format mp3 "${timestamp}_%02d.mp3"
+params=${params:-""}
+map=${map:-"0:0"}
+avconv -i "$radio_stream" -t 3600 -c:a libmp3lame -b:a $output_bitrate $params -map $map -f segment -segment_time 300 -segment_format mp3 "${timestamp}_%02d.mp3"
