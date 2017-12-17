9b875c3813904a02878e0ebaa22ba4a8d96927cf
Adriaan <adriaanvanrossum@users.noreply.github.com>
2017-12-16 19:09:09 -0500

    Update stream URL of SLAM! with redirect and no video

--- radio/slamfm.sh
+++ radio/slamfm.sh
@@ -3,9 +3,7 @@
 source /root/cronjobs/tools/init.sh
 
 radio_name="slamfm"
-radio_stream="https://hls.slam.nl/streaming/hls/SLAM%21/playlist.m3u8"
-params="-vn" # Skip video completely
-map="0:1" # Get second stream (which is audio)
+radio_stream="http://playerservices.streamtheworld.com/api/livestream-redirect/SLAM_MP3.mp3"
 output_bitrate="128k"
 
 source /root/cronjobs/tools/record.sh
